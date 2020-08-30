69951cd2d9551c80a7bb44eaea8ff6b2998d2e58
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-30 18:55:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page 
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
   
   
@@ -23,6 +24,8 @@ class Scraper
   end
   
 end
+
+
 
 
 
